7da9eddaad3ff4e84a219839675a372ef6ec6c79
luscas <lcky123hop@gmail.com>
2022-07-25 22:22:34 -0300

    Desenvolvento Controller do usuario

---
+++ src/main/java/com/am/hambuqueria/api/controller/AberturaController.java
@@ -0,0 +1,13 @@
+package com.am.hambuqueria.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AberturaController {
+    @GetMapping
+    public String imobiliaria() {
+        String url = "https://github.com/LucasFelip";
+        return "BEM VINDO A API DE IMOBILIARIA                    → Desenvolvido por Lucas Felipe " + url;
+    }
+}
