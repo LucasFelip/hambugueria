4b3df99780539bb2fe012e228da7b4c0c427567a
Lucas Felipe <lcky123hop@gmail.com>
2022-07-30 09:39:55 -0300

    Conexão ao banco

--- src/main/java/com/am/hambuqueria/api/controller/UsuarioController.java
+++ src/main/java/com/am/hambuqueria/api/controller/UsuarioController.java
@@ -1,8 +1,46 @@
 package com.am.hambuqueria.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UsuarioController {
+import com.am.hambuqueria.api.dto.mapper.UsuarioMapperDTO;
+import com.am.hambuqueria.api.dto.response.UsuarioResponseDTO;
+import com.am.hambuqueria.domain.model.Usuario;
+import com.am.hambuqueria.domain.service.UsuarioService;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = "/usuario")
+public class UsuarioController {
+    private final UsuarioMapperDTO mapper;
+    private final UsuarioService service;
+
+    // ESSA CLASSE TA ERRADA ( Return erro bad request code 500)
+    @GetMapping
+    public ResponseEntity<UsuarioResponseDTO> listar() {
+        Usuario usuario = (Usuario) service.todos();
+        final UsuarioResponseDTO usuarioResponseDTO = mapper.toResponse(usuario);
+        return new ResponseEntity<>(usuarioResponseDTO, HttpStatus.OK);
+    }
+
+    /*
+     * @GetMapping("{id}")
+     * public Optional<UsuarioResponseDTO> listaUsuarioPorId(@RequestParam(value =
+     * "id") long id) {
+     * List<Usuario> usuario = service.buscaPor(id);
+     * List<UsuarioResponseDTO> usuarioResponse = mapper.toResponseList(usuario);
+     * return usuarioResponse;
+     * }
+     */
 }
